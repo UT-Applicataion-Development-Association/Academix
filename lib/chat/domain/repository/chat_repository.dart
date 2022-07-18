d434ac5baabf354da27940fff7156adf4212c9db
Changhao Song <changhao.song@mail.utoronto.ca>
2022-07-18 16:20:00 -0400

    Add chat feature

---
+++ lib/chat/domain/repository/chat_repository.dart
@@ -0,0 +1,5 @@
+abstract class ChatRepository {
+  const ChatRepository();
+
+  Future<int?> someFunctionName({required String s});
+}
---
+++ lib/chat/domain/services/chat_service.dart
@@ -0,0 +1,10 @@
+import '../repository/index.dart';
+
+class ChatService implements ChatRepository {
+
+  @override
+  Future<int?> someFunctionName({required String s}) async {
+    // TODO: implement function
+    throw UnimplementedError();
+  }
+}
