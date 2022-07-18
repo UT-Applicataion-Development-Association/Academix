b2d05ffc9852478505eb87a3df19ebe149ee1256
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2022-07-18 19:33:55 -0400

    Two static pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:academix_flutter/login_signup/screens/index.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,11 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LoginScreen(),
+      routes: {
+        "/signup": (context) => const SignupScreen(),
+        "/login": (context) => const LoginScreen(),
+      }
     );
   }
 }
