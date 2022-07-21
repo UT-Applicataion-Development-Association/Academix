474f0cfac908eb107cfa93b690ec00fdd9ede161
Zhenfeng <987441105@qq.com>
2022-07-21 19:49:40 -0400

    create appBar in chat screen

--- lib/chat/screens/chat_screen.dart
+++ lib/chat/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -6,6 +7,24 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.red,
+        leading: IconButton(
+          icon: const Icon(CupertinoIcons.search),
+          onPressed: () {},
+        ),
+        title: const Text("Friends(#OfNewMsg)"),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 6),
+            child: IconButton(
+              icon: const Icon(CupertinoIcons.add_circled),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
       body: Center(
         child: Text('chat_screen'),
       ),
